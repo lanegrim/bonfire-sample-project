0d9ddc10d86bcd261228aaad8aad583c6fc6c7fc
lanegrimaud <lanewgrimaud@gmail.com>
2021-08-06 16:01:18 -0400

    content boxes now being dynamically created with JS

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
 $(() => {
+    const populateContentBoxes = (num) => {
+        for (let i = 1; i <= num; i++) {
+            let $contentBox = $('<section></section>', {
+                'class': 'content-box',
+                'id': `content-box-${i}`
+            })
+            let $contentBoxTitle = $(`<h2></h2>`, {
+                'class': 'box-title',
+                'html': `BOX NUMBER ${i}`
+            })
+            let $contentBoxSubtitle = $('<p></p>', {
+                'class': 'box-subtitle',
+                'html': 'USELESS SUBTEXT'
+            })
+            let $contentBoxLinkArea = $('<div></div>', {
+                'class': 'box-link-area'
+            })
+            let $contentBoxLink = $('<a></a>', {
+                'class': 'box-link',
+                'href': '#',
+                'html': 'VIEW THE CASE STUDY'
+            })
 
-    console.log("JS linked")
+            $contentBoxLinkArea.append($contentBoxLink)
+            $contentBox.append($contentBoxTitle)
+            $contentBox.append($contentBoxSubtitle)
+            $contentBox.append($contentBoxLinkArea)
+            $('#content-boxes').append($contentBox)
+        }
+    }
 
+    populateContentBoxes(6);
 })
