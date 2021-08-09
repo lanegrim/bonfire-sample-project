d1846350a481e2e29ac2e4b8b31c65e4f2aad0eb
lanegrimaud <lanewgrimaud@gmail.com>
2021-08-09 11:06:44 -0400

    added styles for large desktop screens

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ $(() => {
             $('#content-boxes').append($contentBox)
         }
     }
-    populateContentBoxes(6);
+    populateContentBoxes(11);
 
     // FILTER CONTENT
     let filterContentBoxes = (filterCriteria) => {
